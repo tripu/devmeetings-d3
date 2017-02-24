5aa0bdf745acd8d7f36a3992ac32f1204d5afcdb
Jorge Aguirre <jorge.aguirre.leon@gmail.com>
2017-02-24 23:35:48 +0100

    Minor enhancements

--- sprint-00/editor/slide-08/app.js
+++ sprint-00/editor/slide-08/app.js
@@ -7,7 +7,7 @@ let svg = d3.select('#main-content').select('svg');
 // 7/ Típicamente no vendrá un array de String, sino un JSON
 const datos = [
   { color: 'steelblue', radius: '25'},
-  { color: 'turqouise', radius: '25'},
+  { color: 'turquoise', radius: '25'},
   { color: 'steelblue', radius: '30'},
   { color: 'gray', radius: '15'},
   { color: 'steelblue', radius: '20'}
